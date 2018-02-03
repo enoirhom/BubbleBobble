e61b9d443f942da12c23488e7866e19bd0a65bfb
Hashtag <emil@MacBook-Pro-de-Emil.local>
2018-02-03 19:34:53 +0100

    Revert "01/02/18"
    This reverts commit e3607d89adb4bb2f7348e5f00b0f194b1d690099.

--- structures.c
+++ structures.c
@@ -1,40 +1,9 @@
-#include <stdbool.h>
+
 
 typedef struct {
   int posX;
   int posY;
   int speedX;
   int speedY;
-  Hitbox hitbox;
-  Pixels sprites[];
+  Pixels sprite[];
 } Player;
-
-
-typedef struct {
-  int posX;
-  int posY;
-  int speedX;
-  int speedY;
-  Hitbox hitbox;
-  Pixels sprites[];
-  bool isTraped;
-  bool isAlive;
-} Enemy;
-
-
-typedes struct {
-  int posX;
-  int posY;
-  int points;
-  int timer;
-} Collectable;
-
-
-int main() {
-  Player player;
-  Enemy enemies [4];
-  Collectable collectable[20];
-  int score = 0;
-  int lives = 3;
-  int level = 1;
-}
