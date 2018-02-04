0c33da867ffb3fe6004d7011f33a829f4a30fd0a
Hashtag <emil@MacBook-Pro-de-Emil.local>
2018-02-04 19:08:13 +0100

    04.02.18 Collisions

--- test.c
+++ test.c
@@ -8,118 +8,162 @@
 #include <stdio.h>
 
 
-void changeSize(int width, int height) {
-    if (height == 0) {
-        height = 1;
-    }
-    float ratio = (float) width / height;
+typedef struct {
+  float x;
+  float y;
+  float size;
+  float ySpeed;
+  float xSpeed;
+} Player;
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glViewport(0, 0, width, height);
-    gluPerspective(45, ratio, 1, 1000);
 
-    glMatrixMode(GL_MODELVIEW);
+typedef struct {
+  float x;
+  float y;
+  float width;
+  float height;
+} Plateform;
+
+
+int width = 600, height = 600;
+Player player;
+Plateform ground[2];
+
+
+void changeSize(int w, int h) {
+  width = w;
+  height = h;
+  ground[0].width = width;
+  glViewport (0, 0, (GLsizei)width, (GLsizei)height);
+
+  glMatrixMode (GL_PROJECTION);
+  glLoadIdentity ();
+  glOrtho(0, width, 0, height, 0, 100);
+
+  glMatrixMode (GL_MODELVIEW);
+  glLoadIdentity ();
+
 }
 
-float angleZ = 0.0f, angleX = 0.0f, red = 0.0f, green = 0.0f, blue = 0.0f;
+
+void drawRect(float x, float y, float width, float height) {
+  glLoadIdentity ();
+  glTranslatef(x,y, 0.0f);
+  glBegin (GL_POLYGON);
+  glVertex2f(0.0f, 0.0f);
+  glVertex2f(width, 0.0f);
+  glVertex2f(width, height);
+  glVertex2i(0.0f, height);
+  glEnd();
+}
+
+
+void movePlayer() {
+  player.ySpeed -= 0.5f;
+  player.y = player.y + player.ySpeed;
+
+  if(player.ySpeed < 0) {
+    for(int i = 0; i < 2; i++) {
+      if(player.y <= ground[i].y + ground[i].height && player.y >= ground[i].y + ground[i].height - 2.0f) {
+        player.y = ground[i].y + ground[i].height;
+        player.ySpeed = 0.0f;
+      }
+    }
+  }
+
+  player.x += player.xSpeed;
+  if(player.xSpeed < 0.0f) {
+    player.xSpeed += 1.0f;
+  } else if (player.xSpeed > 0.0f) {
+    player.xSpeed -= 1.0f;
+  }
+}
+
+
+void physics(int timer) {
+  glutTimerFunc(16, physics, ++timer);
+
+  movePlayer();
+
+  glutPostRedisplay();
+}
+
 
 void renderScene(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    glLoadIdentity();
-    gluLookAt(0.0f, 0.0f, 10.0f,
-              0.0f, 0.0f,  0.0f,
-              0.0f, 1.0f,  0.0f);
+  glColor3f(0.9f, 0.9f, 0.9f);
+  drawRect(ground[0].x, ground[0].y, ground[0].width, ground[0].height);
+  drawRect(ground[1].x, ground[1].y, ground[1].width, ground[1].height);
+  glColor3f(1.0f, 0.6f, 0.0f);
+  drawRect(player.x, player.y, player.size, player.size);
 
-    glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
-    glRotatef(angleX, 0.0f, 1.0f, 0.0f);
-
-    glColor4f(red, green, blue, 0.5f);
-    glBegin(GL_POLYGON);
-        glVertex3f(-5.0f, -5.0f, -5.0f);
-        glVertex3f(-5.0f, 5.0f, -5.0f);
-        glVertex3f(5.0f, 5.0f, -5.0f);
-        glVertex3f(5.0f, -5.0f, -5.0f);
-    glEnd();
-
-
-
-    glutSwapBuffers();
+  glFlush();
 }
+
 
 void processNormalKeys(unsigned char key, int x, int y) {
-    switch(key) {
-        case 27:
-            exit(0);
-        case 66:
-            red = 0.0f;
-            green = 0.0f;
-            blue = 0.0f;
-            break;
-        case 98 :
-            red = 0.0;
-            green = 0.0;
-            blue = 1.0;
-            break;
-        case 103 :
-            red = 0.0;
-            green = 1.0;
-            blue = 0.0;
-            break;
-        case 114 :
-            red = 1.0;
-            green = 0.0;
-            blue = 0.0;
-            break;
-        case 119:
-            red = 1.0f;
-            green = 1.0f;
-            blue = 1.0f;
-            break;
-        default:
-            printf("%i ", key);
-    }
+  switch(key) {
+    case 27:
+      exit(0);
+    default:
+      printf("%c", key);
+  }
 }
+
 
 void processSpecialKeys(int key, int x, int y) {
-    switch (key) {
-        case GLUT_KEY_RIGHT:
-            angleZ += 1.0f;
-            break;
-        case GLUT_KEY_LEFT:
-            angleZ -= 1.0f;
-            break;
-        case GLUT_KEY_UP:
-            angleX += 1.0f;
-            break;
-        case GLUT_KEY_DOWN:
-            angleX -= 1.0f;
-            break;
-    }
+  switch(key) {
+    case GLUT_KEY_RIGHT:
+      player.xSpeed = 12.0f;
+      break;
+    case GLUT_KEY_LEFT:
+      player.xSpeed = -12.0f;
+      break;
+    case GLUT_KEY_UP:
+      player.ySpeed += 10.0f;
+      break;
+    case GLUT_KEY_DOWN:
+      player.y -= 1.0f;
+      break;
+  }
 }
 
+
 int main(int argc, char **argv) {
+  player.x = 100.0f;
+  player.y = 200.0f;
+  player.size = 20.0f;
+  player.ySpeed = 0.0f;
+  player.xSpeed = 0.0f;
 
-    for(int i = 0; i < 128; i++) {
-        printf("%i: %c\n", i, i);
-    }
-    // init GLUT and create Window
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(100,100);
-    glutInitWindowSize(640,640);
-    glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+  ground[0].x = 0.0f;
+  ground[0].y = 0.0f;
+  ground[0].width = width;
+  ground[0].height = 20.0f;
 
-    // register callbacks
-    glutDisplayFunc(renderScene);
-    glutReshapeFunc(changeSize);
-    glutIdleFunc(renderScene);
-    glutKeyboardFunc(processNormalKeys);
-    glutSpecialFunc(processSpecialKeys);
+  ground[1].x = 0.0f;
+  ground[1].y = 400.0f;
+  ground[1].width = width;
+  ground[1].height = 20.0f;
 
-    // enter GLUT event processing cycle
-    glutMainLoop();
+  // init GLUT and create Window
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(width, height);
+  glutCreateWindow("Bubble Bobble");
 
-    return 0;
+  // register callbacks
+  glutDisplayFunc(renderScene);
+  glutReshapeFunc(changeSize);
+  glutTimerFunc(0, physics, 0);
+  glutKeyboardFunc(processNormalKeys);
+  glutSpecialFunc(processSpecialKeys);
+
+  // enter GLUT event processing cycle
+  glutMainLoop();
+
+  return 0;
 }
